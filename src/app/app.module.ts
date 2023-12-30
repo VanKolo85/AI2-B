adadc8f83b8ceba9d2acaa8a6b6a93af95fc3164
VanKolo85 <mateusz.kolosz@gmail.com>
2023-12-30 02:42:42 +0100

    lab-b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HomeComponent} from "./home/home.component";
-import {provideRouter, RouterModule} from "@angular/router";
-import routeConfig from "./routes";
+import { RandomComponent } from './random/random.component';
+import { ListComponent } from './list/list.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule,
-    HomeComponent,
-    RouterModule,
+    BrowserModule, FormsModule
   ],
-  providers: [provideRouter(routeConfig)],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
